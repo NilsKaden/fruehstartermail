b9056f806ed4d293939a12da75702b19b004d9b0
Nils Kaden <nils.kaden94@gmail.com>
2018-02-09 03:20:07 +0000

    fixed faulty redirect after add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
-      redirect_to '/users/index'
+      redirect_to users_url
     else
       render 'new'
     end
