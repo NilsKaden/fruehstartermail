026b65c29be8c865a447c64462151135cf2ae4bb
Nils Kaden <nils.kaden94@gmail.com>
2018-02-09 02:37:59 +0000

    Unlock Gemfile

---
+++ config/initializers/scheduler.rb
@@ -0,0 +1,20 @@
+require 'rufus-scheduler'
+
+s = Rufus::Scheduler.singleton
+
+puts "Initialized Rufus-Scheduler"
+
+
+unless defined?(Rails::Console) || File.split($0).last == 'rake'
+
+  # only schedule when not running from the Ruby on Rails console
+  # or from a rake task
+
+  s.every '1m' do
+    puts "hello, it's #{Time.now}"
+  end
+  
+  
+  ## this will be changed to a daily check for a birthday
+  ## or a check every 29 minutes to keep the process running on heroku
+end
