c904910f16fe4783c8ddfd07b88d37f9123fbedb
Nils Kaden <nils.kaden94@gmail.com>
2018-02-09 05:03:14 +0000

    refactor to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     before_save {self.email = email.downcase }
-    attr_accessor :birthday_string
+    after_save {self.update(:birthday_string => self.birthday.strftime("%d %m"))
     
     validates :firstname, presence: true
     validates :lastname, presence: true
