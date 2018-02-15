c260cad21e07f9c65e123730f0402edb94ac7768
Nils Kaden <nils.kaden94@gmail.com>
2018-02-15 21:29:52 +0000

    added groups model

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,41 @@
+class GroupsController < ApplicationController
+  http_basic_authenticate_with name: "jasper", password: ENV["APP_PASSWORD"]
+  
+  def update
+    @group = Group.find(params[:id])
+    if @group.update_attributes(group_params)
+      redirect_to groups_url
+    else
+      render 'edit'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @groups = Group.all
+    @group = Group.new
+  end
+
+  def destroy
+    Group.find(params[:id]).delete
+    redirect_to groups_url
+  end
+  
+  def create
+    @group=Group.new(group_params)
+    if @group.save
+      redirect_to groups_url
+    else
+      redirect_to groups_url
+    end
+  end
+  
+  private
+  
+    def group_params
+      params.require(:group).permit(:name, :subject, :content)
+    end
+end
