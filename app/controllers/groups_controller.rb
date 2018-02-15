bb1f158302f2d203c1800ec140703041fb746451
Nils Kaden <nils.kaden94@gmail.com>
2018-02-15 22:31:58 +0000

    links2

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -33,6 +33,15 @@ class GroupsController < ApplicationController
     end
   end
   
+  def create
+    @group=Group.new(group_params)
+    if @group.save
+      redirect_to groups_url
+    else
+      redirect_to groups_url
+    end
+  end
+  
   private
   
     def group_params
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ApplicationMailer
   def birthday_mail(user)
     @user = user
 
-    mail to: user.email, subject: "Alles Gute!", bcc: "geburtstag@fruehstarter.net"
+    mail to: user.email, subject: Group.find(user.group_id).subject, bcc: "geburtstag@fruehstarter.net"
 
   end
 
