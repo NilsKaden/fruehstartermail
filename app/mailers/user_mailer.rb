1854182719ff2d09cdf9cccfdeffa41649b9aaf7
Nils Kaden <nils.kaden94@gmail.com>
2018-02-15 23:16:52 +0000

    eval

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,3 @@
-require "erb"
 
 class UserMailer < ApplicationMailer
 
@@ -10,7 +9,7 @@ class UserMailer < ApplicationMailer
   def birthday_mail(user)
     @user = user
 
-    mail to: user.email, body: Erb.new.Group.find(user.group_id).content, subject: Group.find(user.group_id).subject, bcc: "geburtstag@fruehstarter.net"
+    mail to: user.email, body: eval(Group.find(user.group_id).content), subject: Group.find(user.group_id).subject, bcc: "geburtstag@fruehstarter.net"
 
   end
 
