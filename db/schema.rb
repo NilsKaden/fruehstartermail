f9f191643eccafb2aecbf420aa564413bd4f8a01
Nils Kaden <nils.kaden94@gmail.com>
2018-03-09 09:36:05 +0000

    added new fields to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209201842) do
+ActiveRecord::Schema.define(version: 20180309093053) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20180209201842) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "datum"
+    t.boolean "activator"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180209201842) do
     t.datetime "updated_at", null: false
     t.string "birthday_string"
     t.integer "group_id"
+    t.boolean "female"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["group_id"], name: "index_users_on_group_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+first = Group.create!(
+    name: "Freunde",
+    subject: "Alles Gute!",
+    content: "<p>Hi %%%,</p> <p>alles Liebe zum Geburtstag!</p> <p>Jasper</p>",
+    datum: Date.today,
+    activator: false)
+
+
+first.users.create!(
+    firstname: "Nils",
+    lastname: "Kaden",
+    email: "nils.kaden@me.com",
+    birthday: Date.new(1994, 12, 1),
+    female: false)
